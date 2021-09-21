110cbf6eb136f55aaf3c215141266846e5a043f4
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-21 16:02:19 +0000

    Added typewriter function

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() =>{
+    if (i >= sentence.length - 1) {
+      process.stdout.write(sentence[i] + '\n');
+    } else {
+      process.stdout.write(sentence[i]);
+    }
+  }, i*50);
+}
